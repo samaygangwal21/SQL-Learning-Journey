66deb434f213fe893ce79166ca6b7a264364bae3
samaygangwal21 <samaygangwal21@gmail.com>
2025-08-03 19:42:28 +0530

    Add files via upload

---
+++ 1.SELECT_statement.sql
@@ -0,0 +1,8 @@
+SELECT first_name,
+last_name ,
+age,
+age+10 
+FROM parks_and_recreation.employee_demographics;
+
+select DISTINCT gender , first_name
+FROM parks_and_recreation.employee_demographics;
